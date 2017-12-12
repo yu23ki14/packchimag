57e84255a0b6cc09b24a09659c20ea99e901cc6c
yuki kawabe <yuki_021423@yahoo.co.jp>
2017-12-12 11:44:09 +0000

    fix test ckeditor picture

--- app/uploaders/ckeditor_picture_uploader.rb
+++ app/uploaders/ckeditor_picture_uploader.rb
@@ -8,6 +8,7 @@ class CkeditorPictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::ImageScience
 
   # Choose what kind of storage to use for this uploader:
+  
   storage :file
 
   # Override the directory where uploaded files will be stored.
