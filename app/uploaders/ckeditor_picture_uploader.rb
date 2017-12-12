e9f065f0d82ded503d76e182bd9b63d5ed456287
yuki kawabe <yuki_021423@yahoo.co.jp>
2017-12-12 05:24:05 +0000

    set uploader

--- app/uploaders/ckeditor_picture_uploader.rb
+++ app/uploaders/ckeditor_picture_uploader.rb
@@ -8,13 +8,7 @@ class CkeditorPictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::ImageScience
 
   # Choose what kind of storage to use for this uploader:
-  case Rails.env
-    when 'production'
-      storage :dropbox
-
-    when 'development'
-      storage :file
-  end
+  storage :file
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
