307f0bab6ffa9e28a22b98140cfc6023515211ec
yuki kawabe <yuki_021423@yahoo.co.jp>
2017-12-15 15:16:37 +0000

    newest articles

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @articles = Article.order("created_at DESC").last(4)
+    @articles = Article.order("created_at").last(4)
     @month = Month.last
   end
 end
